0a1e34a583d67f42d208db87e82478848efd162a
dankang123 <dongkang@umass.edu>
2023-01-25 21:27:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// start with `npm i axios body-parser cors dotenv express mongoose`
+
+/* 
+axios: HTTP client
+body-parser: body parsing middleware
+cors: CrossOriginResources middleware
+dotenv: loads environment variables
+express: web application framework for Node
+mongoose: mongoDB modeling tool
+
+nodemon: automatically refreshes node app when changes are made
+concurrently: runs multiple commands concurrently
+*/
